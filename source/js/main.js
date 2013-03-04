a802c5ad4a2d690b95799a4d552adb84233e5678
koala <asd5640252@gmail.com>
2013-03-04 18:25:48 +0800

    designing

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,33 @@
-(function($){
+// main
+;(function($) {
+  var $about = $('header .about'),
+      $window = $(window);
+
+  var canToggle = true;
+  $(function() {
+    $('#toggle').on('click', function() {
+      canToggle = false;
+      $about.slideToggle('1000', 'linear', function() {});
+    });
+
+    $window.on('resize', toggleAbout);
+  });
+
+  var toggleAbout = function() {
+    if(canToggle) {
+      if($(window).width() >= 920) {
+        $about.show();
+      } else {
+        $about.hide();
+      }
+    }
+  };
+
+  toggleAbout();
+})(jQuery);
+
+// Gallery
+;(function($) {
   // Caption
   $('.entry').each(function(i){
     $(this).find('img').each(function(){
